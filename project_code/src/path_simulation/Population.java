da0e9dc5a1d8fdbb2285148bbc67c17c2c854cc6
Joao Rodrigues <joaorodrigues481@gmail.com>
2018-05-02 18:13:13 +0100

    small changes

--- project_code/src/path_simulation/Population.java
+++ project_code/src/path_simulation/Population.java
@@ -1,17 +1,28 @@
 package path_simulation;
+import java.util.LinkedList;
 /*
  * A Population has a the current size, the max size and a method that applies an Epidemics to the current Population
  */
 class Population {
 	int size;
 	int maxSize;
+	LinkedList<Individual> indList;
 	
 	Population(int _size, int _maxSize){
 		size=_size;
 		maxSize=_maxSize;
+		indList = new LinkedList<Individual>();
 	}
 	
-	void Epidemics() {
+	void addIndividual(Individual ind) {
+		indList.add(ind);
+	}
+	
+	void remIndividual(Individual ind) {
+		indList.remove(ind);
+	}
+	
+	void epidemics() {
 		
 	}
 }
