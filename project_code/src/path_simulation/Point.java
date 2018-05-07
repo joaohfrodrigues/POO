a0483d5e21fee45dbeb29d9468d8cdefe41e6b05
Joao Rodrigues <joaorodrigues481@gmail.com>
2018-05-07 18:06:25 +0100

    added Move simulateEvent

--- project_code/src/path_simulation/Point.java
+++ project_code/src/path_simulation/Point.java
@@ -1,5 +1,6 @@
 package path_simulation;
 import java.util.Arrays;
+import java.util.Random;
 /*
  * Class Point: has the row, column and the edges vector [up left down right]
  * 
@@ -25,6 +26,26 @@ class Point {
 	
 	int getDistance(Point p) {
 		return Math.abs(this.column-p.column) + Math.abs(this.row-p.row);
+	}
+	
+	int getRandomDir() {
+		int ret=-1;
+		int count=0;
+		Random r = new Random();
+		
+		for(int i=0; i<4;i++)
+			if(edges[i]!=0)
+				count++;
+		/*count is now a value between 0 and count*/ 
+		count=r.nextInt()%count;
+		
+		while(ret==-1) {
+			if(edges[count]!=0)
+				ret=edges[count];
+			count++;
+		}
+		
+		return ret;
 	}
 	
 	@Override
